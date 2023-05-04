45664c986ea30576d5889546b077547e125de410
Your Name <you@example.com>
2023-05-03 21:38:54 -0300

    ..

--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -5,13 +5,19 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function ContactScreen() {
   const [nomeDaPessoa, setNomeDaPessoa] = useState("")
+
   useEffect(() => {
+    // pega o usuário do AsyncStorage
     AsyncStorage.getItem("usuario")
+      // resolve a promessa
       .then((retorno) => {
+        // transforma o JSON em objeto
         const usuario = JSON.parse(retorno)
-        setNomeDaPessoa(usuario.nome)
+        // seta o nome da pessoa na variável de estado
+        setNomeDaPessoa(usuario.nomeDaPessoa)
       })
   }, [])
+
   return (
     <View
       style={styles.container}
