48a7a2d0d1d19699e60c9c0cd3c312cbc7db87cd
Your Name <you@example.com>
2023-05-03 21:15:53 -0300

    Home screen com saudação

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,52 @@
 import { Image, Text, View } from "react-native";
 import styles from "../utils/styles";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../config/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export default function HomeScreen() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user)
+        setUser({
+          userUID: user.uid,
+        })
+      } else {
+        console.log("Não há usuário logado")
+      }
+    })
+    // forma utilizada para ser chamada ao desmontar o componente
+    return () => unsub();
+
+  }, []);// graças a este colchete que simbila a entrada do componente na tela
+
+  useEffect(() => {
+    if (!user?.userUID) return
+
+    const usuariosRef = collection(db, "usuarios");
+
+    const q = query(
+        usuariosRef,
+        where("userUID", "==", user.userUID)
+      );
+    
+    getDocs(q)
+      .then((querySnapshot) => {
+        if(querySnapshot.empty) return
+
+        const usuario = querySnapshot.docs[0].data();
+
+        setUser(usuario)
+
+      })
+
+  }, [user?.userUID]);
+
   return (
     <View style={styles.container}>
       <View>
@@ -12,7 +57,7 @@ export default function HomeScreen() {
           style={styles.image}
         />
       </View>
-      <Text>Hola, bem vindo ao meu app</Text>
+      <Text>Hola {user?.nomeDaPessoa}, bem vindo ao meu app</Text>
       <Aviso />
     </View>
   );
